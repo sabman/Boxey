65dc331ff2071ec6371cb5d64198f7cec9ca8c34
sabman <saburq@gmail.com>
2013-05-07 09:10:21 +0200

    add button

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -5,6 +5,15 @@ class AppDelegate
     @blue_view = UIView.alloc.initWithFrame(CGRect.new([10, 10], [100, 100]))
     @blue_view.backgroundColor = UIColor.blueColor
     @window.addSubview(@blue_view)
+
+    # button
+    @add_button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    @add_button.setTitle("Add", forState:UIControlStateNormal)
+    @add_button.sizeToFit
+    @add_button.frame = CGRect.new(
+        [10, @window.frame.size.height - 10 - @add_button.frame.size.height],
+        @add_button.frame.size)
+    @window.addSubview(@add_button)
     true
   end
 end
